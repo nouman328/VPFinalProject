962b92f3c02b3e7d05872198c95c0bb5538009f4
nouman328 <muhammadnouman328@gmail.com>
2016-04-13 23:47:54 +0500

    webcam access in form based application

---
+++ WinFormCharpWebCam/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Collections.Generic;
+using WebCam_Capture;
+
+
+
+namespace WinFormCharpWebCam
+{
+    //Design by Pongsakorn Poosankam
+    public partial class mainWinForm : Form
+    {
+        public mainWinForm()
+        {
+            InitializeComponent();
+        }
+        WebCam webcam;
+        private void mainWinForm_Load(object sender, EventArgs e)
+        {
+            webcam = new WebCam();
+            webcam.InitializeWebCam(ref imgVideo);
+        }
+
+        private void bntStart_Click(object sender, EventArgs e)
+        {
+            webcam.Start();
+        }
+
+        
+
+        
+    }
+}
